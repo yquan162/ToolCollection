43ac6467709e0ec7d53a80eab2b7cc85711b6507
yquan162 <33588085+yquan162@users.noreply.github.com>
2018-04-13 20:42:40 -0500

    prototype payment algo

--- ToolCollection.java
+++ ToolCollection.java
@@ -397,6 +397,9 @@ class Tools implements Runnable, KeyListener{
         th=gh*1000;
         ph=th*1000;
         eh=ph*1000;
+        final double outerconst, innerconst;
+        outerconst=.64;
+        innerconst=1/64;
         prevTime = timeDiff; 
         boolean refreshScreen=false;
         new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
@@ -453,14 +456,13 @@ class Tools implements Runnable, KeyListener{
         }
         if(!stop)
         {
+            double timeMins = ((((double)System.currentTimeMillis()-(double)callTime)/(double)1000)/(double)60);
             new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            System.out.println("Valid Hash: "+ current+ " Target: 0x"+target+" Diff: "+literalHash.dec2hex(new BigInteger(diff+""))+" Time until solution in minutes: "+df.format((((double)System.currentTimeMillis()-(double)callTime)/(double)1000)/(double)60));
+            System.out.println("Valid Hash: "+ current+ " Target: 0x"+target+" Diff: "+literalHash.dec2hex(new BigInteger(diff+""))+" Time until solution in minutes: "+df.format(timeMins));
+            System.out.println("CLM Owed: "+ outerconst*(innerconst*((diff*0.1)*((((double)System.currentTimeMillis()-(double)callTime)/(double)1000)/(double)60)))+"\n");
+            System.out.println("Payment Token: "+sha256(sha256(diff+"")+sha256(timeMins+""))+" Do not lose this or your CLM cannot be rewarded.");
             //.64(1/64((Diff*0.1)*ElapsedTime)) = CLM Owed
             /*
-            final double outerconst, innerconst;
-            outerconst=.64;
-            innerconst=1/64;
-            System.out.println("CLM Owed: "+ outerconst*(innerconst*((diff*0.1)*((((double)System.currentTimeMillis()-(double)callTime)/(double)1000)/(double)60)))+"\n");
             */
         }
 
